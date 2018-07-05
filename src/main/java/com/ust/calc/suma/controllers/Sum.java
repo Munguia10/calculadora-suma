abf8d74ab969d75ff8eb7d2ebf7d51879febaaef
silverin <sergio_conclave@hotmail.com>
2018-07-05 12:34:55 +0200

    Feature reorganizado

--- src/main/java/com/ust/calc/suma/controllers/Sum.java
+++ src/main/java/com/ust/calc/suma/controllers/Sum.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.ust.calc.suma.controllers;
 
 public interface Sum {
 	
--- src/test/java/controllers/Calculadora_SumaTest.java
+++ src/test/java/controllers/Calculadora_SumaTest.java
@@ -1,26 +1,27 @@
 package controllers;
-
 import static org.junit.Assert.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.ust.calc.suma.controllers.Calculadora_Suma;
+import com.ust.calc.suma.controllers.Sum;
+
 public class Calculadora_SumaTest {
 	
-	private Sum Calculadora_Suma;
+	private Sum calculadora_Suma;
 
 	@Before
 	public void setUp() throws Exception {
 		
-		Calculadora_Suma = new Calculadora_Suma();
+		calculadora_Suma = new Calculadora_Suma();
 	}
 
 	@Test
 	public void test_ok() {
 		
-		final Integer sum = Calculadora_Suma.doSum(3, 2);
+		final Integer sum = calculadora_Suma.doSum(3, 2);
 		
 		assertThat(sum).isEqualTo(5);
 		
@@ -29,7 +30,7 @@ public class Calculadora_SumaTest {
 	@Test
 	public void test_ko() {
 		
-		final Integer sum = Calculadora_Suma.doSum(3, 2);
+		final Integer sum = calculadora_Suma.doSum(3, 2);
 		
 		assertThat(sum).isNotEqualTo(0);
 		
