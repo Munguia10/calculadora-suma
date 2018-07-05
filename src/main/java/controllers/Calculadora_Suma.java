8ba5132d7d82f5422323a1e1d62f09085369aeea
silverin <sergio_conclave@hotmail.com>
2018-07-05 11:36:25 +0200

    Feature a ver si ahora...

--- src/main/java/controllers/Calculadora_Suma.java
+++ src/main/java/controllers/Calculadora_Suma.java
@@ -2,14 +2,9 @@ package controllers;
 
 public class Calculadora_Suma implements Sum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public Integer doSum(Integer val1, Integer val2) {
 		// TODO Auto-generated method stub
-		return val1+val2; //Devuelve la suma
+		return val1+val2; 
 	}
 
 }
