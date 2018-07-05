741fe8837473160db5f2f0a9f5e354299d98959c
silverin <sergio_conclave@hotmail.com>
2018-07-05 11:34:29 +0200

    Feature interfaz implementada

--- src/main/java/controllers/Calculadora_Suma.java
+++ src/main/java/controllers/Calculadora_Suma.java
@@ -1,10 +1,15 @@
 package controllers;
 
-public class Calculadora_Suma {
+public class Calculadora_Suma implements Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Integer doSum(Integer val1, Integer val2) {
+		// TODO Auto-generated method stub
+		return val1+val2; //Devuelve la suma
 	}
 
 }
