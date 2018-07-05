1ab2d4103c16c96f1ccf8d2beaef3d94cebb82c5
silverin <sergio_conclave@hotmail.com>
2018-07-05 11:04:29 +0200

    Feature suma cambios hechos

---
+++ src/main/java/com/ust/calc/suma/SumaApplication.java
@@ -0,0 +1,12 @@
+package com.ust.calc.suma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumaApplication.class, args);
+	}
+}
